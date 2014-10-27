d39fa8736e58661bcfe1a20375c079e00e6780e2
unknown <croftnotworking@gmail.com>
2014-10-27 12:59:26 +0000

    started work three hours ago

--- js/app.js
+++ js/app.js
@@ -1,9 +1,62 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('angularfire-login-boilerplate', ['ngRoute', 'firebase']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/main', {templateUrl: 'partials/main.html', controller: 'MainCtrl'});
-    $routeProvider.when('/dressing', {templateUrl: 'partials/dressing.html', controller: 'DressingCtrl'});
-    $routeProvider.otherwise({redirectTo: '/main'});
-  }]);
+var mainApp = angular.module('mainApp', 
+	['firebase', 'ui.router','ngResource','ngAnimate' ]);
+
+// mainApp.config(['$routeProvider', function($routeProvider) {
+//     $routeProvider.when('/main', {templateUrl: 'partials/main.html', controller: 'MainCtrl'});
+//     $routeProvider.when('/dressing', {templateUrl: 'partials/dressing.html', controller: 'DressingCtrl'});
+//     $routeProvider.otherwise({redirectTo: '/main'});
+//   }]);
+
+mainApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', 
+    function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider, $uiViewScrollProvider, $anchorScrollProvider ) {
+
+    // console.log($uiViewScrollProvider);
+    // $uiViewScrollProvider.useAnchorScroll();
+    // $anchorScrollProvider.disableAutoScrolling();
+    // Public routes
+    
+	var home = { 
+	    name: 'home',  //mandatory
+	    templateUrl: 'js/home/home-layout.html',
+	};
+	var homeLayout = { 
+	    name: 'home.layout',  //mandatory
+	    url: '/',
+	    views:{
+            'welcome':{
+                templateUrl:'js/home/home-welcome.html',
+            },
+            'pitch': {
+                templateUrl: 'js/home/home-pitch.html',
+            }
+        }
+	};
+	var listings = { 
+	    name: 'listings',  //mandatory
+	    templateUrl: 'js/listings/listings-layout.html',
+	};
+	var listingsLayout = { 
+	    name: 'listings.layout',  //mandatory
+	    url: '/listings',
+	    views:{
+            'main':{
+                template:'js/listings/listings-main.html',
+            },
+            'sidebar': {
+                template: 'js/listings/listings-sidebar.html',
+            }
+        }
+	};
+
+	$stateProvider
+	  .state(home)
+	  .state(homeLayout)
+	  .state(listings)
+	  .state(listingsLayout)
+	;
+
+
+}]);
