b6c2c89d08a06c22d00f7212702a378f7dc3f071
unknown <croftnotworking@gmail.com>
2014-10-27 21:32:26 +0000

    this is looks like a knife and it talks about cutting and patterns

--- js/profile/controllers/DreamsCtrl.js
+++ js/profile/controllers/DreamsCtrl.js
@@ -4,7 +4,8 @@ var DreamsCtrl = angular.module('DreamsCtrl', []);
 DreamsCtrl.controller('DreamsCtrl', ['$scope', function($scope) {
   $scope.user ={
 		'name': 'urbanquack:',
-		'message': 'I want to cut patterns'
+		'coverletter': '<h3>I want to cut patterns</h3><p>Or something. Designing and manufacturing clothes is what I do best and it is what I love to do. I only have the experience I have given myself so I do not expect to be put in the design department from day one.</p>',
+		'cv': '<h3>A wisywig object</p>'
 	} 
 }]);
 
