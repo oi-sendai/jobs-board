674cd9347b8d4605594f46464627b1a53748ba21
unknown <croftnotworking@gmail.com>
2014-10-28 15:20:21 +0000

    model bootrapped with AuthCtrlSystem

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on filters, and services
 var SystemApp = angular.module('SystemApp', 
 	['firebase', 'ui.router','ngResource','ngAnimate','ui.bootstrap'
+	,'AuthCtrlSystem'
 	,'HeaderCtrl'
 	,'HeaderDirective'
 	,'FooterCtrl'
---
+++ js/system/auth/AuthCtrl.js
@@ -0,0 +1,44 @@
+var AuthCtrl = angular.module('AuthCtrlSystem', []);
+
+
+
+AuthCtrl.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
+
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'js/system/auth/auth-modal.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+});
+
+AuthCtrl.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+});
