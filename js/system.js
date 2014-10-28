2a7d3efd759ad12408b8f6e33b00baa1d0def4de
unknown <croftnotworking@gmail.com>
2014-10-28 18:49:35 +0000

    user registration added

---
+++ js/system.js
@@ -0,0 +1,154 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+var SystemApp = angular.module('SystemApp', 
+	['ui.router','ngResource','ngAnimate','ui.bootstrap','ngRoute','firebase'
+	,'AuthCtrlSystem'
+	// ,'MainCtrl'
+	,'HeaderCtrl'
+	,'HeaderDirective'
+	,'FooterCtrl'
+	,'FooterDirective'
+	,'ProfileCtrl'
+	,'DreamsCtrl'
+	,'SkillsCtrl'
+	,'StatsCtrl'
+	,'ListingsCtrl'
+	]);
+
+
+SystemApp.config(['$stateProvider', '$urlRouterProvider', 
+    function ($stateProvider, $urlRouterProvider ) {
+    
+	var home = { 
+	    name: 'home',  //mandatory
+	    templateUrl: 'js/home/home-layout.html',
+	};
+	var homeLayout = { 
+	    name: 'home.layout',  //mandatory
+	    url: '/',
+	    views:{
+            'welcome':{
+                templateUrl:'js/home/home-welcome.html',
+            },
+            'pitch': {
+                templateUrl: 'js/home/home-pitch.html',
+            }
+        }
+	};
+	var homeLayout = { 
+	    name: 'home.register',  //mandatory
+	    url: '/register',
+	    views:{
+            'welcome':{
+                templateUrl:'js/system/register.html',
+            },
+        }
+	};
+	var listings = { 
+	    name: 'listings',  //mandatory
+	    templateUrl: 'js/listings/listings-layout.html',
+	};
+	var listingsLayout = { 
+	    name: 'listings.layout',  //mandatory
+	    url: '/listings',
+	    views:{
+            'main':{
+                templateUrl:'js/listings/listings-main.html',
+                controller: 'ListingsCtrl' 
+            },
+            'sidebar': {
+                templateUrl: 'js/listings/listings-sidebar.html',
+                controller: 'ListingsCtrl' 
+            }
+        }
+	};
+	var listingsShow = { 
+	    name: 'listings.show',  //mandatory
+	    url: '/listings/:listing_id',
+	    views:{
+            'main':{
+                templateUrl:'js/listings/listings-main.html',
+                controller: 'ListingsCtrl' 
+            },
+            'sidebar': {
+                templateUrl: 'js/listings/listings-sidebar.html',
+                controller: 'ListingsCtrl' 
+            }
+        }
+	};
+	var profile= { 
+	    name: 'profile',  //mandatory
+	    templateUrl: 'js/profile/profile-layout.html',
+	    controller: 'ProfileCtrl'
+	};
+	var profileView = { 
+	    name: 'profile.view',  //mandatory
+	    url: '/:username',
+	    views:{
+            'dreams':{
+                templateUrl:'js/profile/profile-dreams.html',
+                controller: 'DreamsCtrl'
+            },
+            'stats':{
+            	templateUrl: 'js/profile/profile-stats.html',
+                controller: 'StatsCtrl'
+            },
+            'skills': {
+            	templateUrl: 'js/profile/profile-skills.html',
+                controller: 'SkillsCtrl'
+
+            }
+        }
+	};
+	var profileViewExperience = { 
+	    name: 'profile.view.experience',  //mandatory
+	    url: '/experience',
+	    views:{
+            'dreamsInner':{
+                templateUrl:'js/profile/tabs/profile-experience.html',
+                controller: 'DreamsCtrl'
+            },
+        }
+	};
+	var profileViewIntroduction = { 
+	    name: 'profile.view.introduction',  //mandatory
+	    url: '/introduction',
+	    views:{
+            'dreamsInner':{
+                templateUrl:'js/profile/tabs/profile-introduction.html',
+                controller: 'DreamsCtrl'
+            },
+        }
+	};
+	var profileEdit = { 
+	    name: 'profile.edit',  //mandatory
+	    url: '/profile/i-want-to-cut-patterns', // :profilename
+	    views:{
+            'dreams':{
+                template:'js/profile/edit-profile-dreams.html',
+            },
+            'stats':{
+            	template: 'js/profile/edit-profile-stats.html' 
+            },
+            'skills': {
+                template: 'js/profile/edit-profile-skills',
+            }
+        }
+	};
+
+	$stateProvider
+	  .state(home)
+	  .state(homeLayout)
+	  .state(listings)
+	  .state(listingsLayout)
+	  .state(listingsShow)
+	  .state(profile)
+	  .state(profileView)
+	  .state(profileViewIntroduction)
+	  .state(profileViewExperience)
+	  .state(profileEdit)
+	;
+
+
+}]);
--- js/system/auth/AuthCtrl.js
+++ js/system/auth/AuthCtrl.js
@@ -1,8 +1,8 @@
-var AuthCtrl = angular.module('AuthCtrlSystem', []);
+var AuthCtrlSystem = angular.module('AuthCtrlSystem', []);
 
 
 
-AuthCtrl.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
+AuthCtrlSystem.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
 
   $scope.items = ['item1', 'item2', 'item3'];
 
@@ -27,7 +27,7 @@ AuthCtrl.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
   };
 });
 
-AuthCtrl.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+AuthCtrlSystem.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
 
   $scope.items = items;
   $scope.selected = {
@@ -42,3 +42,102 @@ AuthCtrl.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items
     $modalInstance.dismiss('cancel');
   };
 });
+
+AuthCtrlSystem.controller("AuthCtrl", function($scope, $http,angularFire, angularFireAuth) {
+    // now we can use $firebase to synchronize data between clients and the server!
+  $scope.loginBusy = false;
+  $scope.userData = $scope.userData || {};
+
+  console.log('got this far');
+  var ref = new Firebase('https://brilliant-fire-7870.firebaseio.com/');
+  angularFireAuth.initialize(ref, {scope: $scope, name: 'user'});
+  console.log('scope');
+  /*//////////////LOGIN - LOGOUT - REGISTER////////////////////*/
+  
+  $scope.login = function() {
+    $scope.loginMessage = "";
+    if ((angular.isDefined($scope.inputEmail) && $scope.inputEmail != "") && (angular.isDefined($scope.inputPassword) && $scope.inputPassword != "")) {
+      $scope.loginBusy = true;
+      angularFireAuth.login('password', {
+        email: $scope.inputEmail,
+        password: $scope.inputPassword
+      });
+    } else {
+      $scope.loginMessage = "Please enter a username and password!";
+    }
+  };
+  
+  $scope.logout = function() {
+    $scope.loginBusy = true;
+    $scope.loginMessage = "";
+    $scope.greeting = "";
+    $scope.disassociateUserData();
+    $scope.userData = {};
+    angularFireAuth.logout();
+  };
+
+  $scope.register = function() {
+    $scope.loginMessage = "";
+    if ((angular.isDefined($scope.inputEmail) && $scope.inputEmail != "") && (angular.isDefined($scope.inputPassword) && $scope.inputPassword != "")) {
+      $scope.loginBusy = true;
+      angularFireAuth.createUser($scope.inputEmail, $scope.inputPassword, function(err, user) {
+        if (user) {
+          console.log('New User Registered');
+        }
+        $scope.loginBusy = false;
+      });
+    } else  {
+      $scope.loginMessage = "Please enter a username and password!";
+    }
+  };
+  
+  $scope.$on('angularFireAuth:login', function(evt, user) {
+    $scope.loginBusy = false;
+    $scope.user = user;
+    console.log("User is Logged In");
+    angularFire(ref.child('users/' + $scope.user.id), $scope, 'userData').then(function(disassociate) {
+      $scope.userData.name = $scope.userData.name || {};
+      if (!$scope.userData.name.first) {
+        $scope.greeting = "Hello!";
+      } else {
+        $scope.greeting = "Hello, " + $scope.userData.name.first + "!";
+      }
+      $scope.disassociateUserData = function() {
+        disassociate();
+      };
+    });
+  });
+  
+  $scope.$on('angularFireAuth:logout', function(evt) {
+    $scope.loginBusy = false;
+    $scope.user = {};
+    console.log('User is Logged Out');
+  });
+  
+  $scope.$on('angularFireAuth:error', function(evt, err) {
+    $scope.greeting = "";
+    $scope.loginBusy = false;
+    $scope.loginMessage = "";
+    console.log('Error: ' + err.code);
+    switch(err.code) {
+      case 'EMAIL_TAKEN':
+        $scope.loginMessage = "That email address is already registered!";
+        break;
+      case 'INVALID_PASSWORD':
+        $scope.loginMessage = "Invalid username + password";
+    }
+  });
+});
+
+
+// // a factory to create a re-usable profile object
+// // we pass in a username and get back their synchronized data
+// SystemApp.factory("Profile", ["$firebase", function($firebase) {
+//   return function(username) {
+//     // create a reference to the user's profile
+//     var ref = new Firebase("https://brilliant-fire-7870.firebaseio.com/profiles/").child(username);
+
+//     // return it as a synchronized object
+//     return $firebase(ref).$asObject();
+//   }
+// }]);
