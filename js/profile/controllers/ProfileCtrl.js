a8279abe6cc38f74034c74ce340c9cc2bf654dd7
unknown <croftnotworking@gmail.com>
2014-10-28 10:44:29 +0000

    some feature request planning from correct dir

--- js/profile/controllers/ProfileCtrl.js
+++ js/profile/controllers/ProfileCtrl.js
@@ -2,8 +2,5 @@ var ProfileCtrl = angular.module('ProfileCtrl', []);
 
 ProfileCtrl.controller('ProfileCtrl', ['$scope', '$stateParams', function($scope, $stateParams) {
 	console.log('ProfileCtrl');
-	$scope.username = $stateParams.username;
-	console.log($stateParams);
-	console.log($scope.username);
 }]);
 
