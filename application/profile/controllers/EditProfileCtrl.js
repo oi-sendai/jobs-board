5b9e6f28f76bc24b66493e91a6a2f7899d42f8d1
unknown <croftnotworking@gmail.com>
2014-10-31 12:27:31 +0000

    some styles

--- application/profile/controllers/EditProfileCtrl.js
+++ application/profile/controllers/EditProfileCtrl.js
@@ -1,5 +1,5 @@
 // 'use strict';
-var EditProfileCtrl = angular.module('EditProfileCtrl', []);
+var EditProfileCtrl = angular.module('EditProfileCtrl', ['ngAnimate']);
 
 EditProfileCtrl.controller('EditProfileCtrl', function(
     $resource,
@@ -12,6 +12,30 @@ EditProfileCtrl.controller('EditProfileCtrl', function(
     $scope.debug = 'js/listing/EditProfileCtrl';
     $scope.class = 'account';
     $scope.formData = {};
+
+    $scope.editswitch =  false;
+    $scope.user = 	{ 
+		"username":"me",
+		"name"   : { 
+			"last" : "Kafka", 
+			"first" : "Franz"
+		}, 
+		"profile": { 
+			"Introduction" : "I know it might affect my art if I leave my Brotberuf but I really think I could be doing something better with my time",
+			"Experience" : "Just a guy who writes short stories after work",
+		},
+		"stats": {
+			"location" : "Prague, Czechoslovakia", 
+			"website"  : "careersuici.de",
+			"rating"   : 6, 
+		},
+		"skills": [
+			{"name":"design", "tooltip": "Studied Product Desing & Innovation at University of Strathclyde"},
+			{"name":"javascript", "tooltip": "MEAN"},
+			{"name":"information architecture", "tooltip": "Is the structural design of shared information environment"}
+		]
+
+	};
 
 
     $scope.createProfile = function(){
